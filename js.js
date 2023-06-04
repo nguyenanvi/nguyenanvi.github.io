cb6a3117984757946fd804b5645828f3ab496357
Nguyen An Vi <nguyenanvi122333@gmail.com>
2023-06-04 17:22:14 +0700

    Optimized

--- js.js
+++ js.js
@@ -4,19 +4,33 @@ function s(selector){
 function ss(selector){
   return document.querySelectorAll(selector);
 }
-document.addEventListener('DOMContentLoaded', ()=>{
-  // remove all .dark-theme
-  ss('.dark-theme').forEach((element)=>{
-    element.classList.remove("dark-theme");
-  });
-
-  s('input[type=checkbox][name=theme]').setAttribute("id","a220401");
-  s('input[type=checkbox][name=theme]').insertAdjacentHTML("beforebegin",'<div class="theme-preview" for="a220401"></label>');
-  s('input[type=checkbox][name=theme]').insertAdjacentHTML("afterend",'<label for="a220401"></label><label for="a220401"></label>');
+async function initSwitcher(selector){
+  const switcher = s(selector);
+  const switcherClasslist = switcher.classList;
+  switcher.parentNode.classList.add('switcher-wrapper');
+  switcher.setAttribute("id","a220401");
+  switcher.insertAdjacentHTML("afterend",`
+    <label class="${switcherClasslist}" for="a220401"></label>
+    <label for="a220401"></label>
+  `);
+  switcher.style.display = "none";
 
   // add change theme click
-  s('input[type=checkbox][name=theme]').addEventListener("click", function(){
-   s('body').classList.toggle('dark-theme')
-   s('.preview').classList.toggle('dark-theme')
+  s(selector).addEventListener("click", function(){
+    ss('.light-theme').forEach(element => {
+      element.classList.add('temp-theme');
+      element.classList.remove('light-theme');
+    });
+    ss('.dark-theme').forEach(element => {
+      element.classList.add('light-theme');
+      element.classList.remove('dark-theme');
+    });
+    ss('.temp-theme').forEach(element => {
+      element.classList.add('dark-theme');
+      element.classList.remove('temp-theme');
+    });
   })
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+  initSwitcher('input[type=checkbox][name=theme]');
 })
